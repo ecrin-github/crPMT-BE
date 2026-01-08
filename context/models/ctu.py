b00baee31d0419d3fa9e1f8afce27c161dcc16b1
cudillal <leopold.cudilla@ecrin.org>
2026-01-08 09:59:11 +0000

    Add manual_add flag to CTU model to differentiate CTUs added from Microsoft list from CTUs added from the UI

--- context/models/ctu.py
+++ context/models/ctu.py
@@ -10,6 +10,7 @@ class CTU(models.Model):
     short_name = models.CharField(max_length=255, blank=True, null=True)
     address_info = models.TextField(blank=True, null=True)
     sas_verification = models.BooleanField(default=False)
+    manual_add = models.BooleanField(default=False)
     country = models.ForeignKey(Country, on_delete=models.CASCADE,
                                     db_column='country_id', blank=True, null=True,
                                     related_name='ctu_country_id', default=None)
